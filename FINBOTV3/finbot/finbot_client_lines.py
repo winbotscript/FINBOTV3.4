f2ddaed0739560f49caea5bce1f70af4dd39c062
ALFINONH <39095075+ALFINONH@users.noreply.github.com>
2018-08-01 00:12:34 +0700

    Add files via upload

---
+++ FINBOTV3/finbot/finbot_client_lines.py
@@ -0,0 +1,10 @@
+restarting...
+pointer...
+server into vps
+wait/"linux","cantos","windows","FinbotV1","vultra"/import .finbot or .finbotpy expel FinbotServer=11 into file microscofe.txt for database
+.finbot import wait poin.services
+.finbot import database.server
+from .finbot, ranger, response, FinbotServer, types, rangerv2, finbotV.04
+import updater from types..FinbotServer..finbot
+from LINE.DEVELOPER import finbotpy
+imposible UTF.8 UIX to PYTHON3
